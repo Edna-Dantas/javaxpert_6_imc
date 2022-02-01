88c8c14192a03477a4c3bc288b905ce95967cec4
Edna <na.edna@gmail.com>
2022-02-01 10:43:25 -0300

    Separação das responsabilidades - Métodos

--- src/imc/Imc.java
+++ src/imc/Imc.java
@@ -1,62 +1,32 @@
 package imc;
 
-import java.util.Scanner;
-
 public class Imc {
 
-	public static void main(String[] args) {
-        
-		// Declaração das variáveis
-		int pesoDoUsuario;
-		double alturaDoUsuario, imc;
-		String nomeDoUsuario;
-	
-		
-		//Ler o teclado do usuário
-		Scanner leitor = new Scanner(System.in); 		
-		
-		System.out.println("Qual o seu nome");
-		nomeDoUsuario = leitor.nextLine();
-		
-		//Mostrar qual é o caractere que está na posição 3
-		System.out.println(nomeDoUsuario.charAt(2));
-		
-		//Criando as variáveis para o cálculo
-		System.out.println(nomeDoUsuario + " digite seu peso: "); //Concatenação
-		pesoDoUsuario = leitor.nextInt();
-		
-		System.out.println("\nDigite a sua altura: ");
-		alturaDoUsuario = leitor.nextDouble();
-					
-		leitor.close();	
-		
-		imc = pesoDoUsuario/(alturaDoUsuario*alturaDoUsuario);
-				
-		//System.out.println("");		
-		System.out.println("\n" + nomeDoUsuario + ", seu imc é " + imc + ".");
-		
-		String email = "na.edna@gmail.com";
-		System.out.println(email.endsWith("com"));
-		System.out.println(email.contains("@"));
-		
-		System.out.println("------------------------------------");
-		
-		System.out.println(nomeDoUsuario.equals("Edna"));
-		System.out.println(nomeDoUsuario.equalsIgnoreCase("edna"));	
+	double calcularImc(int peso, double altura) {
+
+		double imc = peso / (altura * altura);
+
+		return imc;
+	}
+
+	void classificarImc(double imc) {
+
 		
 		// Determinar a classificação do IMC
 		if (imc < 18.5) {
 			System.out.println("Você está ABAIXO DO PESO!!");
 		} else if (imc >= 18.5 && imc < 25) {
-			System.out.println("PESO NORMAL");			
-		
+			System.out.println("PESO NORMAL");
 		} else if (imc >= 25 && imc < 30) {
 			System.out.println("SOBREPESO");
-			
-		}else if (imc >= 30 && imc < 35) {
+		} else if (imc >= 30 && imc < 35) {
 			System.out.println("OBESIDADE GRAU 1");
+		} else if (imc >= 35 && imc < 40) {
+			System.out.println("OBESIDADE GRAU 2");
+		} else {
+			System.out.println("OBESIDADE GRAU 3");
 		}
-				
+
 	}
 
 }
